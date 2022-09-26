7fdab96dab04818b015adf647460f5f9d411d5a0
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-09-26 13:16:41 +0300

    закоментил промежуточный вариант

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,29 +1,26 @@
+// import countryListTpl from '../';
+// import countryInfoTpl from '../';
+import debounce from 'lodash.debounce';
 
 function fetchCountries(name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}`)
-      .then(response => {
-        return response.json();
-      })
-      .then(country => {
-        console.log(country);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  
-    
-  
-    // https://restcountries.com/v2/{service}?fields={field},{field},{field}
-    // https://restcountries.com/v2/all?fields=name,capital,currencies
-  
-    //Тебе нужны только следующие свойства:
-  
-    // name.official - полное имя страны
-    // capital - столица
-    // population - население
-    // flags.svg - ссылка на изображение флага
-    // languages - массив языков
-  }
-  
-  fetchCountries("ukraine");
-  
+  fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    return response.json();
+  });
+
+
+
+  // https://restcountries.com/v2/{service}?fields={field},{field},{field}
+  // https://restcountries.com/v2/all?fields=name,capital,currencies
+}
+
+// fetchCountries('ukraine');
+
+function renderCountryList(country) {
+  const markupList = countryListTpl(country);
+  refs.countryListEl.innerHTML = markupList;
+}
+
+function renderCountryInfo(country) {
+  const markupInfo = countryInfoTpl(country);
+  refs.countryInfoEl.innerHTML = markupInfo;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
 import './css/styles.css';
 import './fetchCountries.js';
-
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-    input: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info'),
+  inputEl: document.querySelector('#search-box'),
+  countryListEl: document.querySelector('.country-list'),
+  countryInfoEl: document.querySelector('.country-info'),
+};
+
+// refs.inputEl.addEventListener(
+//   'input', onSearch
+
+//   debounce(fetchCountries, DEBOUNCE_DELAY)
+// );
+
+// function onSearch(event) {
+//     event.preventDefault();
+
+
+// const form = event.currentTarget;
+// const searchQuery = form.elements.query.value;
+
+
+//   fetchCountries('ukraine')
+//     .then({ renderCountryList, renderCountryInfo })
+//     .catch(error => {
+//       console.log(error);
+//     })
+//     .finally(() => form.reset());
+// }
+
+{
+//   <li class="country-list_item">
+//       <div>
+//         <img src="{{flags.svg}}" alt="flag" />
+//         {{name.oficial}}
+//       </div>
+//     </li>
 }
 
+{
+  /* <li class="country-info_item">
+  <div>
+    <p class="capital">Capital: {{capital}}</p>
+    <p class="population">Population: {{population}}</p>
+    <p class="languages">Languages: 
+      <ul class="languages-list">
+        {{#each}}
+          <li class="languages-item">{{#each languages}}</li>
+        {{/#each}}
+      </ul>
+    </p>
+  </div>
+</li> */
+}
 
+//Тебе нужны только следующие свойства:
+
+// name.official - полное имя страны
+// capital - столица
+// population - население
+// flags.svg - ссылка на изображение флага
+// languages - массив языков
