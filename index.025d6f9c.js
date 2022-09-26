51a6329c27d97f61f36dae56458281c00feed163
VladyslavBudko <104014952+VladyslavBudko@users.noreply.github.com>
2022-09-26 10:17:08 +0000

    Deploying to gh-pages from @ VladyslavBudko/goit-js-hw-10@7fdab96dab04818b015adf647460f5f9d411d5a0 🚀

--- index.025d6f9c.js
+++
@@ -1,2 +0,0 @@
-!function(){fetch("https://restcountries.com/v3.1/name/".concat("ukraine")).then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info")}();
-//# sourceMappingURL=index.025d6f9c.js.map
--- index.0f51ce03.js
+++
@@ -1,2 +0,0 @@
-fetch(`https://restcountries.com/v3.1/name/${"ukraine"}`).then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.0f51ce03.js.map
