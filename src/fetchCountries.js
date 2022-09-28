76c7274a2388beed314571c8ad7086ce15a1ee6d
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-09-28 18:44:45 +0300

    del comment

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,7 +4,6 @@ export { fetchCountries, onFetchError };
 function fetchCountries(name) {
   const base_url = `https://restcountries.com/v3.1/name/${name}`;
   const filter_url = `?fields=name,capital,population,flags,languages`;
-  debugger;
   return fetch(base_url + filter_url)
     .then(response => {
       if (!response.ok) {
@@ -18,11 +17,3 @@ function fetchCountries(name) {
 function onFetchError(error) {
   Notiflix.Notify.failure('Oops, there is no country with that name');
 }
-
-//Тебе нужны только следующие свойства:
-
-// name.official - полное имя страны
-// capital - столица
-// population - население
-// flags.svg - ссылка на изображение флага
-// languages - массив языковl
